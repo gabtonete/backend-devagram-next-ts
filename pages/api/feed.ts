49a70c958379e872894fa194cc47519753d926d4
Gabriel Tonete <gabtopi77@gmail.com>
2022-02-19 10:53:39 -0300

    devagram

---
+++ pages/api/feed.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
+
+const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+    try {   
+        if (req.query.userId) {
+            // agora q tenho o id do usuario
+            // como eu valido se o usuario valido
+            const usuario = await UsuarioModel.findById(req?.query?.userId);
+
+            if (!usuario) {
+                return res.status(400).json({ erro: 'Usuario nao encontrado' });
+            }
+        
+            // e como eu busco as publicacoes dele?
+            const publicacoes = await PublicacaoModel
+                .find({ idUsuario: usuario._id })
+                .sort({ data: -1 });
+
+            return res.status(200).json(publicacoes);
+        }
+
+        return res.status(405).json({ erro: 'Metodo informado nao e valido' });
+
+    } catch (e) {
+        console.log(e);
+    }
+    return res.status(400).json({ erro: 'Nao foi possivel obter o feed' });
+}
+
+export default validarTokenJWT(conectarMongoDB(feedEndpoint));
