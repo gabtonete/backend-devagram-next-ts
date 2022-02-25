76cb3cd1a84e205b0c5190a476bc0e02a0674bec
Gabriel Tonete <gabtopi77@gmail.com>
2022-02-25 17:34:25 -0300

    api comentario

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,10 +4,12 @@ import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import { UsuarioModel } from '../../models/UsuarioModel';
 import { PublicacaoModel } from '../../models/PublicacaoModel';
+import { SeguidorModel } from '../../models/SeguidorModel';
 
 const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
-    try {   
-        if (req.query.userId) {
+    try { 
+        if(req.method === 'GET') {
+            if (req.query.userId) {
             // agora q tenho o id do usuario
             // como eu valido se o usuario valido
             const usuario = await UsuarioModel.findById(req?.query?.userId);
@@ -22,9 +24,41 @@ const feedEndpoint = async (req: NextApiRequest, res: NextApiResponse<RespostaPa
                 .sort({ data: -1 });
 
             return res.status(200).json(publicacoes);
-        }
 
-        return res.status(405).json({ erro: 'Metodo informado nao e valido' });
+            }else{
+            const {userId} = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro : 'Usuario nao encontrado'});
+            }
+
+            const seguidores = await SeguidorModel.find({usuarioId : usuarioLogado._id});
+            const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId);
+
+            const publicacoes = await PublicacaoModel.find({
+                $or : [
+                    {idUsuario : usuarioLogado._id},
+                    {idUsuario : seguidoresIds}
+                ]
+            })
+            .sort({data : -1});
+
+            const result = [];
+            for (const publicacao of publicacoes) {
+               const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+               if(usuarioDaPublicacao){
+                    const final = {...publicacao._doc, usuario : {
+                        nome : usuarioDaPublicacao.nome,
+                        avatar : usuarioDaPublicacao.avatar
+                    }};
+                    result.push(final);  
+               }
+            }
+
+            return res.status(200).json(result);
+        }
+    }
+    return res.status(405).json({ erro: 'Metodo informado nao e valido' });
 
     } catch (e) {
         console.log(e);
