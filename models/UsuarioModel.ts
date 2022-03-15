b68cb96b1cd9b8d4a075045422bd53d317df23b3
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-15 16:52:06 -0300

    [Fix] Avatar model correction

--- models/UsuarioModel.ts
+++ models/UsuarioModel.ts
@@ -4,7 +4,7 @@ const UsuarioSchema = new Schema({
     nome: {type : String, required : true},
     email: {type : String, required : true},
     senha: {type : String, required : true},
-    avatar : {type : String, required : true},
+    avatar : {type : String, required : false},
     seguidores : {type : Number, default : 0},
     seguindo : {type : Number, default : 0},
     publicacoes : {type : Number, default : 0},
